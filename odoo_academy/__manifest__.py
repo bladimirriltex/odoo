1b99f02f50f860ecd244c1fe7789e75e8b9dbeb3
Edu Bladimir <bladimirriltex@gmail.com>
2022-05-17 20:07:16 +0000

    [ADD] Vistas

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -14,6 +14,13 @@
     'category': 'Training',
     'version':'0.1',
     'depends':['base'],
-    'data':[],
-    'demo':[]
+    'data':[
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',
+    ],
+    'demo':[
+        'demo/academy_demo.xml',
+    ],
 }
