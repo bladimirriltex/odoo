e7f52b75bcd5cabe2a889fe2d8b6f9d70b3ab378
Edu Bladimir <bladimirriltex@gmail.com>
2022-05-19 13:36:40 +0000

    [ADD]view inherit

--- odoo_academy/models/sale_order.py
+++ odoo_academy/models/sale_order.py
@@ -1,16 +1,16 @@
-# .*. coding: utf-8 -*-
+# .*- encoding: utf-8 -*-
 
 from odoo import models, fields, api
 
-class SaleOrder(models.Model)
+class SalesOrder(models.Model):
     _inherit = 'sale.order'
-    
+
     session_id = fields.Many2one(comodel_name='academy.session',
-                                 string='Related Session',
-                                 ondelete='set null')
-    
+                                string='Related Session',
+                                ondelete='set null')
+
     instructor_id = fields.Many2one(string='Session Instructor',
-                                    related='session_id.instructor_id')
-    
+                                   related = 'session_id.instructor_id')
+
     student_ids = fields.Many2many(string='Students',
-                                   related='session_id.student_ids')
+                                   related = 'session_id.student_ids')
